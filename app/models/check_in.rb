f7b302b7d0089bd61bebcf6c1a4ff9ecce068d0c
ankitpatnaik <ankit.patnaik91@gmail.com>
2016-05-04 15:47:33 +0000

    Change to Check_in.rb validating email/event

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -3,6 +3,8 @@ class CheckIn < ActiveRecord::Base
     belongs_to :event, :class_name => Event
     
     scope :checkin_list, -> {CheckIn.group(:event_id).count}
+    validates :member_id,:presence => {:message => "Choose an Email"}, :allow_blank => false,:uniqueness => { :case_sensitive => false }
+    validates :event_id,:presence => {:message => "Choose an Event"}, :allow_blank => false,:uniqueness => { :case_sensitive => false }
     
     validates_uniqueness_of :member_id, scope: :event_id, :message => 'Already checked in !!'
 end
